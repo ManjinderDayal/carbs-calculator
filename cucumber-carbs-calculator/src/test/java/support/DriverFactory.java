b7df48ba0a0a236df7d798e31a3fd136db620177
ManjinderDayal <97906850+ManjinderDayal@users.noreply.github.com>
2026-02-26 21:53:33 -0500

    Update DriverFactory.java

--- cucumber-carbs-calculator/src/test/java/support/DriverFactory.java
+++ cucumber-carbs-calculator/src/test/java/support/DriverFactory.java
@@ -3,21 +3,20 @@ package support;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
     public static WebDriver driver;
 
     public static void initDriver() {
-        WebDriverManager.chromedriver().setup();
+
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
+        options.addArguments("--headless=new");   // better than --headless
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
         driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
     }
 
     public static void quitDriver() {
